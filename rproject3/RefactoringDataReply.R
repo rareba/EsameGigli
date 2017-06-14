ecd7eae7e079ac821ac2dabc5b7f73833d2e5359
rareba <vannini.gv@gmail.com>
2017-06-14 11:20:00 +0200

    sono andato avanti

--- rproject3/RefactoringDataReply.R
+++ rproject3/RefactoringDataReply.R
@@ -1,24 +1,33 @@
 # Parte 1 - prendere i dati
 
-library(jsonlite)
-library(urltools)
+library(httr)
 
-address = "1600+Pennsylvania+Avenue,+Washington,+DC"
+WScall <- function(endpoint, parameters) {
+    result <- GET(endpoint, query = parameters)
+    return(result)
+}
 
-urla <- "http://nominatim.openstreetmap.org/search?q=bla&format=json"
-urla <- param_set(url, key = "q", value = address)
+ad <- "1600 Pennsylvania Avenue, Washington, DC"
+urla <- "http://nominatim.openstreetmap.org/search"
+paramA <- list(q = ad, addressdetails = 1, format = "json")
 
-# read url and convert to data.frame
-jsonaddress = fromJSON(readLines(url)[1])
+resA <- WScall(urla, paramA)
 
-lat = jsonaddress[[1]]$lat
-lon = jsonaddress[[1]]$lon
+if (resA$status_code == 200) { 
+adrjson <- content(resA, as = "parsed")
+lat = adrjson[[1]]$lat
+lon = adrjson[[1]]$lon
+}
 
-urlw <- "http://forecast.weather.gov/MapClick.php?lat=1&lon=2&FcstType=json"
-urlw <- param_set(url, key = "lat", value = lat)
-urlw <- param_set(url, key = "lon", value = lon)
+urlw <- "http://forecast.weather.gov/MapClick.php"
+paramW <- list(lat = lat, lon = lon, FcstType = "json")
 
-jsonweather = fromJSON(readLines(urlw)[1])
+resW <- WScall(urlw, paramW)
+
+if (resW$status_code == 200) {
+weajson <- content(resW, as = "parsed")
+weajson$currentobservation
+}
 
 # Parte 2 - giocare con i dati
 
@@ -75,7 +84,7 @@ head(df[df$State_Code == 5])
 
 #joining data
 
-df2 = read_csv('small_data/2013_Gaz_tracts_national.tsv', sep = '\t') # Manca il csv nella cartella di dropbox!
+df2 = read_csv('small_data/2013_Gaz_tracts_national.csv', sep = '\t') # Manca il csv nella cartella di dropbox!
 head(df2)
 df_joined = marge(df1, df2, by = 'GEOID')
 head(df_joined)
@@ -92,3 +101,27 @@ df[order(d$index),]
 
 #per colonna
 df[order('AMT_FHA'),]
+
+# Unique values
+unique(df)
+# df['State_Code'].value_counts().head() - boh?
+# df['State_Code'] .isin(df['State_Code'] .head(3)) .head() - boh?
+
+## Handling missing and NA data
+df[is.na(df)]
+dfnaomit = df[na.omit[df]]
+dfna0 = df[is.na(df)] <- 0
+
+## Manipulating strings
+dfstring <- df[grep("stringa", df$AMT_FHA),]
+
+## Indices in Pandas
+s1 <- c(1, 2, 3)
+names(s1) = c('a', 'b', 'c')
+s2 <- c(3, 2, 1)
+names(s2) = c('c', 'b', 'a')
+s1 + s2
+s3 <- c(3, 2, 1)
+names(s3) = c('c', 'd', 'e')
+s1 + s3
+append(s1, s3)
