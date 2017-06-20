db8ecf26277af41622351c4271d3f72e8bd28354
rareba <vannini.gv@gmail.com>
2017-06-20 13:01:17 +0200

    Perfetto fino a ## Indices in Pandas

--- Gigli/RefactoringDataReply.R
+++ Gigli/RefactoringDataReply.R
@@ -1,4 +1,14 @@
-﻿# Parte 1 - prendere i dati
+﻿# Librerie da installare
+install.packages(httr)
+install.packages(readr)
+install.packages(data.table)
+install.packages(psych)
+install.packages(htmltab)
+install.packages(stringi)
+install.packages(bizdays)
+
+
+# Parte 1 - prendere i dati
 
 library(httr)
 
@@ -45,19 +55,18 @@ df1$animal
 ## Verbs (operations) in Pandas
 ## Loading data (and basic statistics / visualization)
 library(readr)
+library(data.table)
 names = c("State_Code", "County_Code", "Census_Tract_Number", "NUM_ALL", "NUM_FHA", "PCT_NUM_FHA", "AMT_ALL", "AMT_FHA", "PCT_AMT_FHA")
-df = as.data.frame(na.omit(read_csv("~/Visual Studio 2017/Projects/MABIDA2017/Gigli/Management science/Data/fha_by_tract.csv", col_names = names)))
-head(df)
+dt = as.data.table(read_csv("~/Visual Studio 2017/Projects/MABIDA2017/Gigli/Management science/Data/fha_by_tract.csv", col_names = names))
+head(dt)
 
-df$GEOID = with(df, ((as.numeric(Census_Tract_Number) * 100) + (10 ^ 6 * as.numeric(County_Code)) + (10 ^ 9 * as.numeric(State_Code))))
-head(df)
+dt$GEOID = as.character(with(dt, as.numeric(Census_Tract_Number) * 100 + 10 ^ 6 * as.numeric(County_Code) + 10 ^ 9 * as.numeric(State_Code)  ))
+head(dt)
 
 df$GEOID = NULL # ma perche' dovrei droppare una colonna?
-df = df[-c(2, 4),] # droppo righe 2 e 4
+df = dt[-1,]
 
 # Indici su data frame
-library(data.table)
-dt <- as.data.table(df)
 setkey(dt, State_Code, County_Code)
 head(dt)
 data.frame(unclass(summary(dt)))
@@ -65,67 +74,68 @@ data.frame(unclass(summary(dt)))
 str(df)
 
 library(psych)
-describeBy(df, df$PCT_AMT_FHA)
-hist(df$PCT_AMT_FHA, breaks = 50, col = rgb(1, 0, 0, 0.5))
+describeBy(dt, df$PCT_AMT_FHA)
+describeBy(dt)
+hist(dt$PCT_AMT_FHA, col = rgb(1, 0, 0, 0.5))
 
-df$LOG_AMT_ALL = log1p(df$AMT_ALL)
+dt$LOG_AMT_ALL = log1p(df$AMT_ALL)
+hist(dt$LOG_AMT_ALL, col = rgb(1, 0, 0, 0.5))
 
 ## Indexing data frames
 
-head(df$State_Code)
+head(dt$State_Code)
 
-head(c('State_Code', 'County_Code'))
-
+head(dt[,c(State_Code, County_Code)])
+head(dt[, County_Code, keyby = State_Code])
 str(df$State_Code)
 
-df[, 4]
+dt[12550,]
 
-df[3, 'State_Code']
+dt[12550, 'State_Code']
 
-df[0:3, "State_Code":'Census_Tract_Number'] ############## rivedere
+dt[12545:12550, County_Code, keyby = State_Code]
 
-df[3, 0]
+dt[3, 5]
 
-df[0:3, 0:3]
+dt[3:5, 2:4]
 
 #filtering data
 
-head(subset(df, State_Code == 33))
-head(subset(df, State_Code == 33 | Census_Tract_Number == 9613))
-df[State_Code == 33] ########### rivedere
+head(subset(dt, State_Code == 33))
+head(subset(dt, (State_Code == 33) | (Census_Tract_Number == 9613)))
+dt[State_Code == 33]
 
 #joining data
 
-df2 = read_csv('~/Visual Studio 2017/Projects/MABIDA2017/Gigli/Management science/Data/2013_Gaz_tracts_national.csv', sep = '\t')
-df2 = as.data.frame(na.omit(read_tsv("~/Visual Studio 2017/Projects/MABIDA2017/Gigli/Management science/Data/2013_Gaz_tracts_national.tsv", col_names = T)))
-head(df2)
-df_joined = merge(df1, df2, by = 'GEOID') ####################rivedere
-head(df_joined)
+dt2 = as.data.table(read_tsv("~/Visual Studio 2017/Projects/MABIDA2017/Gigli/Management science/Data/2013_Gaz_tracts_national.tsv", col_names = T))
+head(dt2)
+dt_joined = dt[dt2, on = "GEOID"]
+head(dt_joined)
 
 #aggregating data
-dfag = aggregate(df, by = 'USPS')
-dfag
+usps_groups = dt_joined[, sum(ALAND), by = USPS]
+usps_groups
 
 #sorting by indices and columns
 
-d$index <- as.numeric(row.names(df))
-df[order(d$index),]
-
-df[order('AMT_FHA'),]
+dtbystate = dt[order(State_Code)]
+dtbyAMTFHA = dt[order(AMT_FHA)]
 
 # Unique values
-unique(df)
-# df['State_Code'].value_counts().head() - boh?
-# df['State_Code'].isin(df['State_Code'] .head(3)) .head() - boh?
+head(unique(dt))
+nrow(unique(dt))
 
-## Handling missing and NA data
-df[is.na(df)]
-dfnaomit = df[na.omit[df]]
-dfna0 = df[is.na(df)] <- 0
-#usare ZOO
+
+## Handling missing and NA data    --- POTENZIALMENTE ESPANDIBILE
+head(is.na(dt))
+naomit = na.omit(dt)
+dt[is.na(dt)] <- 0
 
 ## Manipulating strings
-dfstring <- df[grep("stringa", df$AMT_FHA),]
+library(stringr)
+library(dplyr)
+
+dtstring = dt_joined %>% filter(str_detect(USPS, "A"))
 
 ## Indices in Pandas
 s1 <- c(1, 2, 3)
@@ -138,27 +148,25 @@ names(s3) = c('c', 'd', 'e')
 s1 + s3
 append(s1, s3)
 
-## Function application and mapping
+## Function application and mapping       ---- VEDERE
 testFunc <- function(a, b) a + b
 apply(dat[, c('x', 'z')], 1, function(y) testFunc(y['z'], y['x']))
 
 
-### Pandas HTML data import example
+### Pandas HTML data import example     
 library(htmltab)
 library(stringi)
-
+require(data.table)
 url <- "http://en.wikipedia.org/wiki/List_of_tallest_buildings_and_structures_in_the_world"
 tallest <- htmltab(doc = url, which = 3)
 tl = t(as.data.table(stri_extract_all(tallest$Coordinates, regex = "-?\\d{1,3}+\\.?\\d{4,6}")))
 tallest$Latitude = tl[, 3]
 tallest$Longitude = tl[, 4]
+rm(tl)
 head(tallest)
 
-cleancord = function(string) {
-pattern = ""
-}
 
-## Pandas Timestamps
+## Pandas Timestamps--- FINIRE
 Sys.setlocale("LC_TIME", "C")
 as.POSIXlt("July 4, 2016", format = "%B %d, %Y")
 as.POSIXlt('Monday, July 4, 2016', format = "%A, %B %d, %Y")
@@ -186,12 +194,5 @@ d <- c("2009-03-07 12:00", "2009-03-08 12:00", "2009-03-28 12:00", "2009-03-29 1
 t1 <- as.POSIXct(d, "America/Los_Angeles")
 cbind(US = format(t1), UK = format(t1, tz = "Europe/London"))
 
-## Multi-indices, stacking, and pivot tables
+## Multi-indices, stacking, and pivot tables       ---- CESTINATO
 
-
-## Plugging into more advanced analytics
-df$AMT_FHA
-setkey(dt, group)
-system.time(dt[, list(mean = mean(age), sd = sd(age)), by = group])
-
-╗┐ / ´╗┐
