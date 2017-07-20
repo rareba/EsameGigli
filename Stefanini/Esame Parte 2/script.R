99655d409dbf1f277e9232336cba097f8fe6c430
rareba <vannini.gv@gmail.com>
2017-07-20 12:35:07 +0200

    Esame stefanini finito

--- Stefanini/Esame Parte 2/script.R
+++ Stefanini/Esame Parte 2/script.R
@@ -1,33 +1,34 @@
 
-####### INSTALLARE PRIMA DI ESEGUIRE
 #install.packages(c("bnlearn", "RBGL", "gRain", "igraph"))
 #source("https://bioconductor.org/biocLite.R")
 #biocLite("RBGL")
 #biocLite("graph")
+#biocLite("Rgraphviz")
 #biocLite("BiocGenerics")
 
 library(bnlearn)
 library(gRain)
 library(igraph)
-
+library(Rgraphviz)
+library(tidyverse)
+library(pcalg)                          
 
 # 1
 load("C:/Users/GiulioVannini/Documents/Visual Studio 2017/Projects/MABIDA2017/Stefanini/Esame Parte 2/insuranceDF.RData")
 nrow(insuranceDF)
-# 10000 osservazioni, 27 variabili tutte di tipo Factor
 names(insuranceDF)
 str(insuranceDF)
-# Non ci sono dati mancanti
 summary(insuranceDF)
 
 # 2.1 
 mod1 <- hc(insuranceDF, score = "bde")
-plot(mod1)
+summary(mod1)
+graphviz.plot(mod1)
 
 # 2.2
 library(dplyr)
-# La rete ha 54 archi orientati
 (arcs <- mod1$arcs)
+nrow(directed.arcs(mod1))
 (directed.arcs(mod1))
 for (node in nodes(mod1)) {
     print(paste0("Nodo ", node))
@@ -37,7 +38,7 @@ for (node in nodes(mod1)) {
         print("senza figli")
     }
 
-# Un unico nodo radice, nessun nodo isolato
+# nodi radici/isolati
 for (node2 in root.nodes(mod1)) {
     print(paste0("Nodo radice ", node2))
     if (length(which(mod1$arcs[, 1] == node2)) == 0)
@@ -52,16 +53,16 @@ bl2 <- cbind(rep("PropCost", length(names(insuranceDF[, -20]))), names(insurance
 bl3 <- cbind(rep("DrivHist", length(names(insuranceDF[, -27]))), names(insuranceDF[, -27]))
 bl <- rbind(bl1, bl2, bl3)
 mod.2 <- hc(insuranceDF, score = "bde", blacklist = bl)
-plot(mod.2)
-# La rete contiene 56 archi orientati
-directed.arcs(mod2)
+graphviz.plot(mod.2)
+nrow(directed.arcs(mod.2))
+directed.arcs(mod.2)
 
 # 4.1
 wl <- rbind(c("SeniorTrain", "ThisCarDam"), c("MedCost", "RuggedAuto"), c("SocioEcon", "GoodStudent"),
           c("Age", "RiskAversion"), c("Age", "ThisCarDam"), c("RuggedAuto", "VehicleYear"))
 mod.3 <- hc(insuranceDF, score = "bic", whitelist = wl)
 plot(mod.3)
-# La rete contiene 45 archi orientati
+nrow(directed.arcs(mod.3))
 directed.arcs(mod.3)
 
 # 4.2
@@ -75,38 +76,57 @@ tk_close(idbn)
 # 5.0
 bnlearn::compare(mod.2, mod.3, arcs = FALSE)
 bnlearn::compare(mod.2, mod.3, arcs = TRUE)
-# Le 2 reti hanno 38 archi uguali
 
 
 # 6
 fit = bn.fit(mod.3, insuranceDF)
 mod.3$arcs[which(mod.3$arcs[, 2] == "Theft")]
-#Conditional probability table for the Theft variable
 (theftProb <- fit$Theft)
 bn.fit.barchart(theftProb)
 bn.fit.dotplot(theftProb)
 
 theftProbab <- as.data.frame(fit$Theft$prob)
 str(theftProbab)
-# probabilità condizionata di Theft, condizionata all'avverarsi di ThisCarCost=="TenThou",ThisCarDam=="Moderate"
 freq <- filter(theftProbab, ThisCarCost == "TenThou", ThisCarDam == "Moderate")
-#bassisima probabilità che venga rubata una macchina del costo del ordine della decina di migliaia di dollari 
-#con danni di entità moderata
 freq
 
 # 7.1
-library(pcalg)
-mod.2
+require(pcalg)
 mod.2.graphnel <- as.graphNEL(mod.2)
-# PropCost non è indipendente da RiskAversion condizionatamente a MakeModel,DrivQuality,Theft
 pcalg::dsep("PropCost", "RiskAversion", c("MakeModel", "DrivQuality", "Theft"), mod.2.graphnel)
 
-
+      
 # 7.2
 mod.2.fit <- bn.fit(mod.2, insuranceDF)
-? simulate
-sim <- bnlearn::simulate(mod.2, nsim = 10000)
-? simulate
+mod2gr <- as.grain(mod.2.fit)
+sim <- simulate(mod2gr, nsim = 10000)
+str(sim)
+
+modsim <- hc(sim, score = "bde")
+d <- graphviz.plot(modsim);
+plot(d, main = "Simulation of Bayesian Network", attrs = list(node = list(fillcolor = "yellow", fontsize = 70),
+            edge = list(color = "black"),
+            graph = list(rankdir = "UD")))
+
+for (var in names(sim)) {
+    print(sim %>%
+          group_by_(var) %>%
+          summarise(n = n()) %>%
+          mutate(freq = n / sum(n)))
+}
 
 # 8
-cptable(vpar, levels = NULL, values = NULL, normalize = TRUE, smooth = 0)
+mod2grain <- as.grain(mod.2.fit)
+MPCarValue <- querygrain(mod2grain, nodes = c("CarValue"))
+MPCarValue[[1]]["FiftyThou"]
+
+ev <- list(Age = "Adult",
+                 RiskAversion = "Psychopath",
+                 AntiTheft = "True",
+                 MakeModel = "SportsCar",
+                 PropCost = "HundredThou")
+
+CarValueCond <- querygrain(mod2grain, nodes = c("CarValue"), evidence = ev)
+CarValueCond[[1]]["FiftyThou"]
+
+MPCarValue[[1]]["FiftyThou"] - CarValueCond[[1]]["FiftyThou"]
